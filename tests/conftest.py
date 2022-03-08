26d66f753ff3ee5b4a9d521fe026ee9f76b9e681
Dmitry Kilchik <dmitry.kylchik@instatsport.email>
2022-03-08 16:11:16 +0200

    driver fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 import pytest
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
-def driver():
-    driver = Chrome()
+def driver(request):
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    driver = Chrome(options=chrome_options)
+    request.cls.driver = driver
     yield driver
     driver.quit()
