567be1526c0ceb78dc62fdceac7317e00344f9f7
Dmitry Kilchik <dmitry.kylchik@instatsport.email>
2022-03-08 21:55:11 +0200

    header widget

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,15 @@
 from utilities.webdriver.web import WebdriverExtension
+from .widgets import Header
 
 class BasePage(WebdriverExtension):
 
+    # TODO adding complete description
+    """
+    This layer is used to interact to the business logic of an product.
+    All webdriver wrappers are taken out to parent layer (WebdriverExtension class) 
+    and can be reused at any projects.
+    """
+
     def __init__(self, driver):
         super().__init__(driver)
+        self.header = Header(self.driver)
