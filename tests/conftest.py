ff8c28198677b599f3a7e3ffea6679660dd1b83c
Dmitry Kilchik <dmitry.kylchik@instatsport.email>
2022-03-09 08:57:48 +0200

    language addoption

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,21 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     # Parameter to run in headless mode
-    parser.addoption('--headless', action='store', default=False,
-                     help='To run in headless mode add to cmd "--headless=True"')
+    parser.addoption("--headless", action="store", default=False,
+                     help="To run in headless mode add to cmd '--headless=True'")
+    # Parameter to set user language
+    parser.addoption("--lang", action="store", default="en-gb", 
+                     help="Set webdriver language")
 
 
 
 @pytest.fixture
 def driver(request):
     chrome_options = Options()
+    user_language = request.config.getoption("lang")
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
     headless_mode = request.config.getoption("headless")
     if headless_mode:
         chrome_options.add_argument('--headless')
